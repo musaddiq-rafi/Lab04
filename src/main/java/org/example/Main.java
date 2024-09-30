7940dc21c9d49bbe171d7034b2649ec21c0149eb
musaddiq-rafi <musaddiq@iut-dhaka.edu>
2024-09-30 09:36:09 +0600

    added system to work with json

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,7 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -12,39 +13,83 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        String filePath = "src/main/java/org/example/BankDetails.txt";
+        String textfilePath = "src/main/java/org/example/BankDetails.txt";
+        String jsonfilePath = "src/main/java/org/example/BankDetails.json";
+
+        // Reading from a text file
+        ArrayList<Transactions> transactionsFromText = readfromTextFile(textfilePath);
+        displayTransactionSummary(transactionsFromText, "Text File");
+
+        //Reading from a json file
+        ArrayList<Transactions> transactionsFromJson = readfromJsonFile(jsonfilePath);
+        displayTransactionSummary(transactionsFromJson, "Json File");
+
+    }
+
+
+
+    private static void displayTransactionSummary(ArrayList<Transactions> transactionsFromText, String textFile) {
+    }
+
+    public static ArrayList<Transactions> readfromTextFile(String filePath){
+            ArrayList<Transactions> transactions= new ArrayList();
+            String line = "";
+            String regex = ",";
+            int transactionCount=0;
+            try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+
+                while ((line = br.readLine()) != null) {
+                    String[] data = line.split(regex);
+
+
+                    String date = data[0];
+                    System.out.println(date);
+                    int amount = Integer.parseInt(data[1]);
+                    System.out.println(amount);
+                    String purpose = data[2];
+                    System.out.println(purpose);
+
+                    Transactions transaction = new Transactions(date, amount, purpose);
+                    transactions.add(transaction);
+
+                    transactionCount++;
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return transactions;
+            }
+
+    private static ArrayList<Transactions> readfromJsonFile(String jsonfilePath) {
         ArrayList<Transactions> transactions = new ArrayList<>();
 
-
-        String line = "";
-        String regex = ",";
-        int transactionCount=0;
-
-        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
-
+        try (BufferedReader br = new BufferedReader(new FileReader(jsonfilePath))) {
+            StringBuilder jsonString = new StringBuilder();
+            String line;
             while ((line = br.readLine()) != null) {
-                String[] data = line.split(regex);
+                jsonString.append(line);
+            }
 
+            JSONArray jsonArray = new JSONArray(jsonString.toString());
 
-                String date = data[0];
-                System.out.println(date);
-                int amount = Integer.parseInt(data[1]);
-                System.out.println(amount);
-                String purpose = data[2];
-                System.out.println(purpose);
+            for (int i = 0; i < jsonArray.length(); i++) {
+                JSONObject jsonObject = jsonArray.getJSONObject(i);
+
+                String date = jsonObject.getString("date");
+                int amount = jsonObject.getInt("amount");
+                String purpose = jsonObject.getString("description");
 
                 Transactions transaction = new Transactions(date, amount, purpose);
                 transactions.add(transaction);
-
-                transactionCount++;
             }
 
         } catch (IOException e) {
             e.printStackTrace();
         }
-        System.out.println("The total number of transactions is: "+ transactionCount);
-        System.out.println("The total profit or loss is: "+ Analysis.profitOrLoss(transactions));
-        System.out.println("The most expensive transaction is: "+ Analysis.mostExpense(transactions));
+
+        return transactions;
     }
 
+
 }
+
