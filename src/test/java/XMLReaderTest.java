ab55c13f6a73848773cf79b4695124d832baf221
musaddiq-rafi <musaddiq@iut-dhaka.edu>
2024-11-19 22:39:52 +0600

    added all the testcases

--- src/test/java/XMLReaderTest.java
+++ src/test/java/XMLReaderTest.java
@@ -3,8 +3,6 @@ package org.example;
 
 import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
-
-import static org.junit.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class XMLReaderTest {
